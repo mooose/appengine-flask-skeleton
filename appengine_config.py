0cfb7a712fbd38b26eea9fe8f7ca3ed3990798ae
Siegfried Hirsch <siegfriedhirsch@Siegfrieds-iMac-2015.local>
2019-01-10 16:25:35 +0100

    Added files to the first version of the skeleton.

---
+++ appengine_config.py
@@ -0,0 +1,21 @@
+from google.appengine.ext import vendor
+
+import os
+
+
+# Add any libraries installed in the "lib" folder.
+vendor.add('lib')
+# vendor.add('libs')
+
+# if os.environ.get('SERVER_SOFTWARE', '').startswith('Development'):
+#     import imp
+#     import os.path
+#     import inspect
+#     from google.appengine.tools.devappserver2.python import sandbox
+
+#     sandbox._WHITE_LIST_C_MODULES += ['_ssl', '_socket']
+#     # Use the system socket.
+
+#     real_os_src_path = os.path.realpath(inspect.getsourcefile(os))
+#     psocket = os.path.join(os.path.dirname(real_os_src_path), 'socket.py')
+#     imp.load_source('socket', psocket)
